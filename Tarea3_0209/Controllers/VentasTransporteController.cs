8250fa57172692966abda4cdac63026b563d8421
Fabrizio951 <fabrizio_zuniga2003@hotmail.com>
2024-06-13 22:01:08 -0500

    Primer commit

---
+++ Tarea3_0209/Controllers/VentasTransporteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tarea3_0209.Models;
+
+namespace Tarea3_0209.Controllers
+{
+    public class VentasTransporteController : Controller
+    {
+        // GET: VentasTransporte
+        public ActionResult ActionTransporte()
+        {
+            return View(new Transporte());
+        }
+
+        [HttpPost]public ActionResult ActionTransporte(Transporte reg) 
+        {
+            ViewBag.costo = reg.Costo().ToString("#####.#0");
+            ViewBag.seguro = reg.Seguro().ToString("#####.#0");
+            ViewBag.monto = reg.Monto().ToString("#####.#0");
+            ViewBag.IGV = reg.IGV().ToString("#####.#0");
+            ViewBag.MontoTotal = reg.MontoTotal().ToString("#####.#0");
+            return View(reg);
+        }
+    }
+}
---
+++ Tarea3_0209/Models/Transporte.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarea3_0209.Models
+{
+    public class Transporte
+    {
+        public string _numero { get; set; }
+        public DateTime _fecha { get; set; }
+        public string _cliente { get; set; }
+        public string _carga { get; set; }
+        public double _TM { get; set; }
+        public double _precio { get; set; }
+        public Transporte()
+        {
+            _numero = "";
+            _fecha = DateTime.Now;
+            _cliente = "";
+            _carga = "";
+            _precio = 150;
+            _TM = 0;
+
+        }
+        public double Costo() { return _precio * _TM; }
+        public double Seguro() { return Costo() * 0.02; }
+        public double Monto() { return Costo() + Seguro(); }
+        public double IGV() { return 0.18 * Monto(); }
+        public double MontoTotal() { return Monto() + IGV(); }
+    }
+}
